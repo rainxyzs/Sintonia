24f902ce6052efb298dfd8973d99b0b0426363f0
BRYAN DAMASO BRASIL <00001118864293sp@aluno.educacao.sp.gov.br>
2026-09-11 07:54:36 -0300

    feat: adiciona PWA (manifest, service worker, ícones)

---
+++ sintonia/sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'sintonia-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys.filter(function (k) { return k !== CACHE; })
+          .map(function (k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (cached) {
+      return cached || fetch(e.request);
+    })
+  );
+});
